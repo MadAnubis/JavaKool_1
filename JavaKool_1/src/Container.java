d9376c0e0462a3ee050f526eac01888cbcfe0473
madanubis <kaz.club@hotmail.com>
2014-12-12 20:44:51 +0200

    a

---
+++ JavaKool_1/src/Container.java
@@ -0,0 +1,18 @@
+
+public class Container {
+	public int value_;
+	public int x_;
+	public int y_;
+	public int last_sum_;
+	public Container last_sum_link;
+	public char link_;
+	public boolean used_;
+	
+	public Container() {
+		value_ = 0;
+		x_ = 0;
+		y_ = 0;
+		last_sum_ = 0;
+		used_ = false;		
+	}
+}
